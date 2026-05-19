85c5fd81e70fe9526a6f3a6429cd66dbd9737075
Nainaru Chiranjeevi <156818135+Nainaru14@users.noreply.github.com>
2026-05-19 12:10:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,164 +1,36 @@
 const roles=[
-
 "Full Stack Developer",
-
 "Java Developer",
-
 "Software Engineer"
-
 ];
 
 let i=0;
 let j=0;
-let current="";
 
 function typing(){
 
-if(i<roles.length){
+let current=roles[i];
 
-current=roles[i];
-
-document.getElementById(
-"typing"
-
-).innerHTML=
-
+document.getElementById("typing")
+.innerHTML=
 current.slice(0,j++);
 
-if(j<=current.length){
-
-setTimeout(typing,100);
-
-}
-
-else{
+if(j>current.length){
 
 j=0;
+
 i++;
 
-setTimeout(typing,1000);
-
-}
-
-}
-
-else{
+if(i>=roles.length){
 
 i=0;
 
+}
+
+}
+
+setTimeout(typing,120);
+
+}
+
 typing();
-
-}
-
-}
-
-typing();
-
-
-
-function showProject(project){
-
-const title=
-document.getElementById(
-"title"
-);
-
-const details=
-document.getElementById(
-"details"
-);
-
-if(project==="sales"){
-
-title.innerHTML=
-"Sales Savvy";
-
-details.innerHTML=`
-
-Tech:
-Java, Spring Boot, JWT,
-Razorpay, REST APIs
-
-Features:
-
-• Authentication
-
-• Product Catalog
-
-• Cart Service
-
-• Order Management
-
-• Payment Gateway
-
-`;
-
-}
-
-
-if(project==="task"){
-
-title.innerHTML=
-"Task Buddy";
-
-details.innerHTML=`
-
-Tech:
-
-React JS, JavaScript
-
-Features:
-
-• CRUD
-
-• React Hooks
-
-• Dynamic UI
-
-`;
-
-}
-
-
-if(project==="door"){
-
-title.innerHTML=
-"Automatic Door Lock";
-
-details.innerHTML=`
-Arduino-based
-security automation system
-using sensors and servo motor.
-`;
-
-}
-
-
-if(project==="vlsi"){
-
-title.innerHTML=
-"Carry Select Adder";
-
-details.innerHTML=`
-Reduced area, delay,
-and power consumption
-using XOR optimization.
-`;
-
-}
-
-
-document.getElementById(
-"modal"
-).style.display="flex";
-
-}
-
-
-function closeModal(){
-
-document.getElementById(
-"modal"
-).style.display="none";
-
-}
